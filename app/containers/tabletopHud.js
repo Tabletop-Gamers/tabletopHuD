99ba0a52ef83d33365b36d1f1128978c33d50713
tlevin <toddlevin802@gmail.com>
2016-02-10 13:23:00 -0800

    Landing page mocked

--- app/containers/tabletopHud.js
+++ app/containers/tabletopHud.js
@@ -13,17 +13,24 @@ class TabletopHud extends Component {
     const {state, actions} = this.props
     return (
       <View style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={require('../assets/img/d20.jpg')}
-      />
-        <Text style={styles.logotext}>
-        Tabletop HUD
-        </Text>
-        <TouchableOpacity style={styles.started}>
-            <Image
-              
-              source={require('../assets/img/getStarted.png')} />
+        <Image
+          style={styles.logo}
+          source={require('../assets/img/logo.png')}
+        />
+        <TouchableOpacity style={styles.logoBtn}>
+          <Text style={styles.logotext}>
+            Create a Character
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.logoBtn}>
+          <Text style={styles.logotext}>
+            Game Manager
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.logoBtn}>
+          <Text style={styles.logotext}>
+            Character Manager
+          </Text>
         </TouchableOpacity>
       </View>
       )
@@ -33,22 +40,30 @@ class TabletopHud extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E0DEDE',
+    backgroundColor: '#3A383A',
     alignItems: 'center',
     justifyContent: 'center'
   },
   logo: {
-    height: 160,
-    width: 160
+    marginTop: -20,
+    height: 180,
+    width: 160,
+    marginBottom: 40
   },
   logotext: {
-    paddingTop: 20,
-    fontSize: 30
+    textAlign: 'center',
+    paddingTop: 5,
+    fontSize: 20,
+    color: '#3A383A'
   },
-  started: {
-    width: 60,
-    height: 60,
-    paddingTop: 50
+  logoBtn: {
+    alignItems: 'center',
+    marginTop: 20,
+    width: 200,
+    height: 40,
+    backgroundColor: '#B6B6B6',
+    borderRadius: 5,
+
   }
 })
 
